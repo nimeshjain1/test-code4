eca65b0c3ea5859bb89ca060ff44148ac4346259
Nimesh Jain <nimesh.jain@gmail.com>
2026-09-07 17:29:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ const port = process.env.PORT || 8080;
 
 const http = require('http');
 
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = { add, subtract };
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({ service: 'test-code4', status: 'ok' }));
